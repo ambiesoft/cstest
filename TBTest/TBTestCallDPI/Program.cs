aa564651484f133bb63914102eacefd54e219bd3
ambiesoft <ambiesoft.trueff@gmail.com>
2022-05-10 19:46:22 +0900

    Add three projects
    Each has different way to test High-DPI

---
+++ TBTest/TBTestCallDPI/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Ambiesoft;
+
+namespace TBTest
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// アプリケーションのメイン エントリ ポイントです。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            CppUtils.AmbSetProcessDPIAware();
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FormMain());
+        }
+    }
+}
